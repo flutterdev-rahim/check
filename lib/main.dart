7712896e7b187427aa38c1c53884e6e3111c0fd3
excelitrahim <rahim@excelitai.com>
2022-07-21 14:50:32 +0600

    rahim

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              ':',
             ),
             Text(
               '$_counter',
@@ -113,3 +113,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+//////rahim/////git 
